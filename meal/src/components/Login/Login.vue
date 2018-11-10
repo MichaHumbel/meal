044472c0545d12a3e3b23a69ed9e8ba3ecda58f0
Justin Spring <justin.spring@swisscom.com>
2018-11-10 16:41:07 +0100

    created Login

---
+++ meal/src/components/Login/Login.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="container">
+
+    <input v-model="email" type="text" placeholder="Email" />
+    <input v-model="password" type="password" placeholder="Password" />
+
+   <button @click="login">login</button>
+    <button @click="redirect">Create Account</button>
+    <br>
+    <br>
+    <div>Login Data: {{loginData}}</div>
+</div>
+</template>
+
+
+
+<style src="./Login.css"></style>
+<script src="./Login.js"></script>
